c6a36002bf0ee0e1c4db19ad1dea655a1cd14142
Zhuofeng-Li <89558990+Zhuofeng-Li@users.noreply.github.com>
2025-09-23 20:47:49 +0000

    Deploying to gh-pages from @ Zhuofeng-Li/Zhuofeng-Li.github.io@89784c6a9475790fa4fe8f1bfb9d5c4e5e0c4379 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
